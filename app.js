86f285703f993d6591af3f01f09788318b6b21e2
Faruq Hossain <faruqdigital01@gmail.com>
2023-05-17 00:13:03 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const auth = 
+'ci9ft5ZOQB7lYFAL4XZmv3iyBLAzwDD1Mv2Lv8K5iNUu94Rnb4EmXk7T'
+
+const gallery = document.querySelector(".gallery")
+const searchInput = document.querySelector(".search-input")
+const searchForm = document.querySelector(".search-form")
+
+console.log(auth);
